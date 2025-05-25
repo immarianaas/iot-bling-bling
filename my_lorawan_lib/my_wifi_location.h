85619d01948f7f380e706650e0f704b2731ea3ba
Mariana <im.marianaas@gmail.com>
2025-05-25 21:59:39 +0200

    add report etc

--- my_lorawan_lib/my_wifi_location.h
+++ my_lorawan_lib/my_wifi_location.h
@@ -3,7 +3,7 @@
 #define MY_WIFI_LOCATION_H
 
 #include "Arduino.h"
-#define WIFI_SEND_INTERVAL 1 * 60 * 1000 // 1 minutes
+#define WIFI_SEND_INTERVAL 10 * 60 * 1000 // 10 minutes
 
 class My_Wifi_Location
 {
